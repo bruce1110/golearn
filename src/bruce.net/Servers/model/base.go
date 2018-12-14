5d21a1c65ccc834eecfcca51082de293f72e31c0
qinchong <qinchong-hj@huajiao.tv>
2018-12-14 19:19:42 +0800

    fix

--- src/bruce.net/Servers/model/base.go
+++ src/bruce.net/Servers/model/base.go
@@ -2,8 +2,10 @@ package model
 
 import "database/sql"
 import _ "github.com/go-sql-driver/mysql"
+import "github.com/garyburd/redigo/redis"
 
 var Db *sql.DB
+var Pool *redis.Pool
 
 const config = "pepper_passport:0b570fc5108d7bf1@tcp(10.138.67.99:2146)/pepper_passport"
 
@@ -19,4 +21,19 @@ func InitMysql() error {
 		return err
 	}
 	return err
+}
+
+func InitRedis() error {
+	Pool = &redis.Pool{
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", "127.0.0.1:6379")
+			if err != nil {
+				return nil, err
+			}
+			return c, nil
+		},
+		MaxIdle:   1,
+		MaxActive: 2,
+	}
+	return nil
 }
